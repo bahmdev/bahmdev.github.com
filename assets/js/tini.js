e9f46dd29cc31897e084f93035e9003abfa6b65a
Bilal Ahmad <bahmdev@gmail.com>
2016-02-21 03:36:05 -0500

    location

--- assets/js/tini.js
+++ assets/js/tini.js
@@ -44,10 +44,10 @@ $( document ).ready(function() {
     Smooch.init({
         appToken: '6xysum4913o3cfreb9rik8dyb',
         customText: {
-            headerText: 'Say Hi! (Terminal)',
+            headerText: 'Say Hi',
             inputPlaceholder: 'Type a message...',
             sendButtonText: 'Send',
-            introText: 'Made with <3 just for you,<br/> Ask me anything!',
+            introText: 'Made with <3 just for you | Ask me anything!',
             settingsText: 'You can leave me your email so that I can get back to you if you want.'
         }
     });
@@ -87,14 +87,33 @@ $( document ).ready(function() {
 
     });
 
+
+    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2-lat1);  // deg2rad below
+        var dLon = deg2rad(lon2-lon1);
+        var a =
+                Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+                Math.sin(dLon/2) * Math.sin(dLon/2)
+            ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c; // Distance in km
+        return d;
+    }
+
+    function deg2rad(deg) {
+        return deg * (Math.PI/180)
+    }
+
     Smooch.on('message:sent', function(message) {
         var rspns = message.text
         var lines = rspns.split(" ");
         var cmd = lines[0];
 
         if (cmd == "/help") {
-            Smooch.sendMessage("=> Available Commands:" +
-                "\n/help\n/resume\n/email\n/python\n/github\n/twitter\n/linkedin\n/togglemusic");
+            Smooch.sendMessage("=> COMMANDS:" +
+                "\n/help\n/resume\n/email\n/python\n/github\n/twitter\n/linkedin\n/togglemusic \<rate\>\n/location");
         } else if (cmd == "/email") {
             Smooch.sendMessage("=> bahmdev@gmail.com");
         } else if (cmd == "/resume") {
@@ -110,14 +129,28 @@ $( document ).ready(function() {
             window.location.href = "https://www.linkedin.com/in/bahmdev";
         } else if (cmd == "/togglemusic") {
             if (audio.paused) {
-
                 if (lines.length > 1)
                     audio.playbackRate = lines[1];
-
                 audio.play();
             }
             else
                 audio.pause();
+        } else if (cmd == "/location") {
+            function showPosition(position) {
+                Smooch.sendMessage("=> Your location is "+position.coords.latitude+ ", "+position.coords.longitude);
+                //Smooch.sendMessage("=> My location is 43.661471, -70.2553259");
+                var d = getDistanceFromLatLonInKm(position.coords.latitude, position.coords.longitude,
+                                                                                                43.661471, -70.2553259);
+                d = Math.round(d * 100) / 100;
+                Smooch.sendMessage("=> We are "+d+"km / "+(d*0.621)+" mi apart.");
+
+            }
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                Smooch.sendMessage("=> My location can't be shared with you.");
+            }
+
         } else if (cmd.charAt(0) == "/"){
             Smooch.sendMessage("=> Invalid Command, type /help for information.");
         }
